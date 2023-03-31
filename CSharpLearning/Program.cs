a1b9fffe2bce35bc52aa5c815e8aa925c96afa0f
Ahmed Elgendy <Ahmed.Elgendy@integrant.com>
2023-03-31 07:04:04 +0200

    Add project files.

---
+++ CSharpLearning/Program.cs
@@ -0,0 +1,15 @@
+﻿using CSharpLearning.Keywords.Modifiers.NewAndOverrid;
+using System;
+
+namespace CSharpLearning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NewAndOverrid_Example1.Execute();
+            NewAndOverrid_Example2.Execute();
+            Console.ReadKey();
+        }
+    }
+}
