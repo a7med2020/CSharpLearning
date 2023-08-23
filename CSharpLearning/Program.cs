9bd0bba8afb159d87ba1c6185265026004ad76a2
Ahmed Elgendy <Ahmed.Elgendy@integrant.com>
2023-08-23 15:09:44 +0300

    Clean Code book Listing 4.7

--- CSharpLearning/Program.cs
+++ CSharpLearning/Program.cs
@@ -1,4 +1,5 @@
-﻿using CSharpLearning.Keywords.Modifiers.NewAndOverrid;
+﻿using CSharpLearning.Books.CleanCode;
+using CSharpLearning.Keywords.Modifiers.NewAndOverrid;
 using System;
 
 namespace CSharpLearning
@@ -8,7 +9,9 @@ namespace CSharpLearning
         static void Main(string[] args)
         {
             //NewAndOverrid_Example1.Execute();
-            NewAndOverrid_Example2.Execute();
+            //NewAndOverrid_Example2.Execute();
+            //GeneratePrimes.generatePrimesAndDisplay(10);
+            PrimeGenerator.generatePrimesAndDisplay(10);
             Console.ReadKey();
         }
     }
