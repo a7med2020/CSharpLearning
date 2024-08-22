093d5971351b46ab9d35fcad690de4448ac1de34
Ahmed Elgendy <Ahmed.Elgendy@integrant.com>
2024-08-23 01:35:51 +0300

    Delegates 1

---
+++ CSharpLearning/Delegates/Callbacks.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpLearning.Delegates
+{
+    class Callbacks
+    {
+    }
+
+    class Program
+    {
+        // Events uses Delegates
+        // Delegates are for callbacks , not encapsulated 
+        // Events publisher subscriber model , encapsulated
+        static void Main(string[] args)
+        {
+            MyFileSearch x = new MyFileSearch();
+            // publisher -- subcriber
+            // observable -- observer
+            x.publisher += Subscriber1;
+            //x.publisher += Subscriber2;
+            //x.SendData = null;
+            Console.WriteLine("File Search started....");
+            Task.Run(() => x.Search("C:\\Windows")); // non blocking call
+            //x.Search("E:\\shivprasad data"); // blocking call
+            Console.WriteLine("Continue executing main function....");
+            Console.ReadLine();
+        }
+        static void Subscriber1(string filename)
+        {
+            Console.WriteLine(filename);
+        }
+        static void Subscriber2(string filename)
+        {
+            Console.WriteLine(filename);
+        }
+    }
+    public class MyFileSearch
+    {
+        public delegate void searchMethod(string search);
+        public event searchMethod publisher = null;
+        //public  searchMethod publisher = null; 
+        public void Search(string dir)
+        {
+            try
+            {
+                foreach (string d in Directory.GetDirectories(dir))
+                {
+                    foreach (string f in Directory.GetFiles(d))
+                    {
+                        publisher(f);
+                        Thread.Sleep(500);
+                    }
+
+
+                }
+            }
+            catch (Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+    }
+}
---
+++ CSharpLearning/Delegates/FunctionPointer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpLearning.Delegates
+{
+    class FunctionPointer
+    {
+        public static void Main()
+        {
+
+            List<Employee> emplist = new List<Employee>();
+
+            emplist.Add(new Employee() { Id = 1, Name = "sivakumar", Experience = 8 });
+            emplist.Add(new Employee() { Id = 2, Name = "Aravind", Experience = 6 });
+            emplist.Add(new Employee() { Id = 3, Name = "jagadeesh", Experience = 10 });
+            emplist.Add(new Employee() { Id = 4, Name = "subbalakshmi", Experience = 9 });
+
+            IsPromotable isPromotable = new IsPromotable(EligibleEmp);
+
+          
+            Employee.PromoteEmployee(emplist, isPromotable);
+
+        }
+
+        public static bool EligibleEmp(Employee ee)
+        {
+            if (ee.Experience >= 8)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+
+    delegate bool IsPromotable(Employee emp1);
+    class Employee
+    {
+
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public int Experience { set; get; }
+
+        public static void PromoteEmployee(List<Employee> emp, IsPromotable IsEligibleToPromote)
+        {
+            foreach (Employee employee in emp)
+            {
+                if (IsEligibleToPromote(employee))
+                {
+                    Console.WriteLine("Employee" + " " + employee.Name + " " + "Promoted");
+                }
+            }
+        }
+
+    }
+}
+
+
+ 
+
